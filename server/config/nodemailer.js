a5fd22e4de56822460c53a893cdd9a3cf6c77c01
Sheezylodhi <muhammadshahzaiblodhi@gmail.com>
2026-06-25 01:42:24 +0500

    //

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -1,10 +1,16 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS // Make sure this is a 16-character App Password
+  },
+  tls: {
+    rejectUnauthorized: false // Render ke liye ye kabhi-kabhi zaroori hota hai
   }
 });
 
