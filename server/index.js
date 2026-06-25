e2482f3b7a5a13d50edc4fed62289addb13b2b5f
Sheezylodhi <muhammadshahzaiblodhi@gmail.com>
2026-06-25 23:41:12 +0500

    ////

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const reservationRoutes = require('./routes/reservationRoutes');
 const app = express();
 
 app.use(cors({
-    origin: ["https://onlineresturantfood.netlify.app", "http://localhost:5173"],
+    origin: ["https://food-ordering-system-blush-zeta.vercel.app", "http://localhost:5173"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
 }));
